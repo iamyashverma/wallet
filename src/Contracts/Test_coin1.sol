6a810090b00cfe6f638e006d324dd86298fb27ed
Yash Verma <yash.verma24071990@gmail.com>
2022-03-14 15:02:15 +0530

    contract

--- src/Contracts/Test_coin1.sol
+++ src/Contracts/Test_coin1.sol
@@ -1,13 +1,12 @@
-// contracts/GLDToken.sol
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Simple_Token is ERC20 {
     mapping(address => address[]) public _history; 
-    mapping(address => int256[]) public _amount; 
-
+    mapping(address => uint256[]) public _amount;
+    mapping(address => bool[]) public _is_sender; 
+    int256 dummy_amt;
 
 
 
@@ -26,17 +25,16 @@ contract Simple_Token is ERC20 {
         _transfer(_msgSender(), recipient, amount);
         
         _history[_msgSender()].push(recipient);
-        
-        _amount[_msgSender()].push(-amount);
+        _amount[_msgSender()].push(amount);
+        _is_sender[_msgSender()].push(true);
         _history[recipient].push(_msgSender());
         _amount[recipient].push(amount);
-
+        _is_sender[recipient].push(false);
         
         return true;
     }
-    function getHistory() public view  returns ( address[] memory add_arr ,  uint256[] memory amount  ) {
-         return (_history[_msgSender()],_amount[_msgSender()]);
-         
+    function getHistory() public view  returns ( address[] memory add_arr ,  uint256[] memory amount, bool[] memory is_sender  ) {
+         return (_history[_msgSender()],_amount[_msgSender()],_is_sender[_msgSender()]);
          }
    
     
